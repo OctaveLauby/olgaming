eca63ede6609dd3133bb82993eb2f9278a0a1106
olauby <octave.lauby@beebryte.com>
2018-05-15 18:26:35 +0200

    Introduce more sophisticated communication tools in player

--- olgaming/player.py
+++ olgaming/player.py
@@ -9,6 +9,7 @@ class Player(GameObject):
         super().__init__(**kwargs)
         self.index = index
         self.requires_visual = False
+        self.last_observation = None
 
     def action(self, gstate, actions=None):
         """Return action of players.
@@ -21,6 +22,48 @@ class Player(GameObject):
             (object) action
         """
         raise NotImplementedError
+
+    def receive(self, msg_dict):
+        """Receiving message dictionary (json frmt)."""
+        self.log.debug("Receiving message : %s" % msg_dict)
+        self.msg_dict_handler(msg_dict)
+
+    def msg_dict_handler(self, msg_dict):
+        """Manage incoming message."""
+
+        # Read purpose of message
+        try:
+            verb = msg_dict['verb']
+        except KeyError:
+            err_msg = "Expecting 'verb' key in message to player"
+            self.log.error(err_msg)
+            raise KeyError(err_msg)
+
+        # Check if a method exists to accomplish purpose
+        try:
+            handler = getattr(self, 'verb_' + verb)
+        except AttributeError:
+            err_msg = "Verb '%s' is not handled by player" % verb
+            self.log.error(err_msg)
+            raise ValueError(err_msg)
+
+        # Gather content to accomplish purpose
+        try:
+            content = msg_dict['content']
+        except KeyError:
+            err_msg = "Expecting 'content' key in message to player"
+            self.log.error(err_msg)
+            raise KeyError(err_msg)
+
+        return handler(content)
+
+    def verb_observe(self, content):
+        """Manage new observation of game."""
+        self.last_observation = content
+
+    def verb_reward(self, content):
+        """Manage reward from game."""
+        self.take(content)
 
     def take(self, consequence):
         """Nothing"""
