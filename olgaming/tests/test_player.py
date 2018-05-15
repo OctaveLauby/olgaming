93897bbd3c4a816a829138beff4a533c371b4159
olauby <octave.lauby@beebryte.com>
2018-05-15 18:39:29 +0200

    Player comm tests

--- olgaming/tests/test_player.py
+++ olgaming/tests/test_player.py
@@ -21,3 +21,31 @@ def test_player():
         player1.action("gstate")
 
     assert player2.take("consequence") is None
+
+    # Test communication
+
+    assert player1.last_observation is None
+    player1.receive({'verb': "observe", 'content': "an_observation"})
+    assert player1.last_observation == "an_observation"
+    player1.receive({'verb': "observe", 'content': "new_observation"})
+    assert player1.last_observation == "new_observation"
+
+    assert player1.last_reward is None
+    player1.receive({'verb': "reward", 'content': 1})
+    assert player1.last_reward == 1
+    player1.receive({'verb': "reward", 'content': 2})
+    assert player1.last_reward == 2
+
+    with pytest.raises(TypeError):
+        player1.receive(12)
+
+    with pytest.raises(ValueError):
+        player1.receive({'verb': "create_a_wormhole"})
+
+    with pytest.raises(KeyError):
+        player1.receive({'verb': "reward", 'value': 12})
+    assert player1.last_reward == 2
+
+    with pytest.raises(ValueError):
+        player1.receive({'verb': "reward", 'content': "a"})
+    assert player1.last_reward == 2
