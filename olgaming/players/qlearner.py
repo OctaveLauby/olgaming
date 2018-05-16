42df5bfa2a6a2bb4474b414a5f3b5e6ab39917eb
olauby <octave.lauby@beebryte.com>
2018-05-16 18:16:07 +0200

    Update qlearner

--- olgaming/players/qlearner.py
+++ olgaming/players/qlearner.py
@@ -4,20 +4,6 @@ import random
 from olgaming.player import Player
 
 
-class Memory(object):
-    def __init__(self, max_size):
-        self.content = []
-        self.max_size = max_size
-
-    def append(self, item):
-        self.content.append(item)
-        if len(self.content) > self.max_size:
-            self.content = self.content[1:]
-
-    def last(self):
-        return self.content[-1]
-
-
 class Qlearner(Player):
     """AI Player using simple qlearning."""
 
@@ -42,48 +28,82 @@ class Qlearner(Player):
             # ...
         }
         self.params = dict(self.dft_params)
-        self.memory = Memory(max_size=1)
+        self.memory = {
+            'stacked_reward': 0,
+            'lst_action': None,
+            'lst_state': None,
+            'new_action': None,
+            'new_state': None,
+        }
 
     # ---- Overridden methods
 
     def action(self, gstate, actions):
+        self.consume_reward(gstate, actions)
+
+        # If state not explored, create it in qvalue
         if gstate not in self.qvalue:
-            self.qvalue[gstate] = {
-                action: 0
-                for action in actions
-            }
+            self.init_state(gstate, actions)
+
+        # Exploration vs exploitation
         if random.random() < self.params['exploration_rate']:
             action = self.random_action(actions)
         else:
             action = self.best_action(gstate, actions)
-        self.memory.append((gstate, action))
+        self.memory['lst_action'] = action
+        self.memory['lst_state'] = gstate
         return action
 
     def take(self, consequence):
-        """"""
-        lst_state, lst_action = self.memory.last()
-        update = (
-            self.params['learning_rate'] * (
-                consequence
-                + (
-                    self.params['discount_rate']
-                    * self.qvalues[next_state][next_action]
-                )
-                - self.qvalues[lst_state][lst_action]
-            )
-        )
-        self.qvalues[lst_state][lst_action] += update
+        self.memory['stacked_reward'] += consequence
 
     # ---- Utils
 
-    def best_action(self, gstate, best_action):
-        bst_action = None
+    def best_action(self, gstate, actions):
+        # Case when gstate hasn't been explore yet : random action
+        if gstate not in self.qvalue:
+            self.init_state(gstate, actions)
+            return self.random_action(actions)
+
+        # Otherwise : random action among actions with best qval
+        bst_actions = []
         bst_qval = - float("inf")
         for action, qval in self.qvalue[gstate].items():
             if bst_qval < qval:
-                bst_action = action
+                bst_actions = [action]
                 bst_qval = qval
-        return bst_action
+            elif bst_qval == qval:
+                bst_actions.append(action)
+        return self.random_action(bst_actions)
+
+    def consume_reward(self, gstate, actions):
+        if self.memory['lst_action'] is None:
+            self.log.debug("No reward to consume")
+            return
+
+        lst_state = self.memory['lst_state']
+        lst_action = self.memory['lst_action']
+        next_action = self.best_action(gstate, actions)
+
+        update = (
+            self.params['learning_rate'] * (
+                self.memory['stacked_reward']
+                + (
+                    self.params['discount_rate']
+                    * self.qvalue[gstate][next_action]
+                )
+                - self.qvalue[lst_state][lst_action]
+            )
+        )
+        self.qvalue[lst_state][lst_action] += update
+
+        self.memory['stacked_reward'] = 0
+
+    def init_state(self, gstate, actions):
+        self.qvalue[gstate] = {
+            action: 0
+            for action in actions
+        }
 
     def random_action(self, actions):
         return random.choice(actions)
